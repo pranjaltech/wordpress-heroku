2ab3ff1028fa3d7a0238c55a2d57b329acdd5a9f
Pranjal Kumar Singh <pranjaltech@gmail.com>
2021-02-11 00:20:20 +0530

    Added two themes

---
+++ assets/wp-content/themes/neve/vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'a18ba0f0a40c34aa289f253297ff69415ace6701',
+    'name' => 'codeinwp/neve',
+  ),
+  'versions' => 
+  array (
+    'codeinwp/neve' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'a18ba0f0a40c34aa289f253297ff69415ace6701',
+    ),
+    'codeinwp/themeisle-sdk' => 
+    array (
+      'pretty_version' => '3.2.16',
+      'version' => '3.2.16.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b116dffbe39ea9e37169fdd683bab54c545fe560',
+    ),
+  ),
+);
